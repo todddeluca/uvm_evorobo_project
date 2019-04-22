06fb62a62c35cff851e7158715b1707b4c1856b4
Todd DeLuca <nospam@todddeluca.com>
2019-04-22 13:24:25 -0400

    Ignore pickles. Sorry about that, pickles.

--- constants.py
+++
@@ -1,8 +0,0 @@
-L = 0.1 # leg length
-R = L / 5 # leg radius
-S = L / 2 # body radius
-eval_time = 1000 # number of timesteps
-pop_size = 1 # population size
-num_gens = 1 # number of generations
-num_envs = 4 # number of environments each individual will be evaluated in
-num_legs = 8
